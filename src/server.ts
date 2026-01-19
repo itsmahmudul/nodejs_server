d78ac01dddca3d67e7f6ac6e6af3bbcb699ed965
itsmahmudul <islamashraf128@gmail.com>
2026-01-19 16:57:01 +0600

    2nd commit

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import config from "./config";
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
     console.log("Server is running");
+
+    //root route
     if (req.url == "/" && req.method == "GET") {
       res.writeHead(200, { "content-type": "application/json" });
       res.end(
@@ -12,6 +14,44 @@ const server: Server = http.createServer(
           path: req.url,
         })
       );
+    }
+
+    //health route
+    if (req.url == "/api" && req.method == "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Health status ok",
+          path: req.url,
+        })
+      );
+    }
+
+    if (req.url === "/api/users" && req.method === "POST") {
+      // res.writeHead(200, { "Content-Type": "application/json" });
+      // return res.end(
+      //   JSON.stringify({
+      //     id: 1,
+      //     name: "alice",
+      //   })
+      // );
+
+      let body = "";
+
+      //listen for data chunk
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on("end", () => {
+        try {
+          const parseBody = JSON.parse(body);
+          console.log(parseBody);
+          res.end(JSON.stringify(parseBody));
+        } catch (err: any) {
+          console.log(err?.message);
+        }
+      });
     }
   }
 );
